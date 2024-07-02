86d16a871de9473a02cc810866d998711bbf38f8
Brian Ding <brian_ding@outlook.com>
2024-07-02 21:53:53 +0800

    Language server (#4)
    * start language server when extension is activated
    
    * use language to select document

--- src/client.ts
+++ src/client.ts
@@ -2,10 +2,21 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { CreateArkTSCommand, HelloWorldCommand } from './commands';
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+// Activating extension 'so-brian.vscode-harmonyos-next' failed:
+// Cannot find module '/home/brian/repo/github.com/so-brian/vscode-harmonyos-next/node_modules/ets-lsp/server.ts'.
+// Please verify that the package.json has a valid "main" entry.
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate (context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -17,7 +28,44 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(new HelloWorldCommand());
 	context.subscriptions.push(new CreateArkTSCommand());
+
+	const serverModule = require.resolve(
+		'ets-lsp'
+	);
+
+	// Debug options for server are used when we launch the extension in debug mode
+	const serverOptions: ServerOptions = {
+		run: { module: serverModule },
+		debug: {
+			module: serverModule,
+			options: { execArgv: ['--inspect=6009'] }
+		}
+	};
+
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [
+			{
+				pattern: '**/*.ets',
+				language: 'ets',
+				scheme: 'file',
+			}
+		],
+	};
+
+	client = new LanguageClient(
+		'vscode-harmonyos-next',
+		'VS Code HarmonyOS Next',
+		serverOptions,
+		clientOptions
+	);
+
+	// Starting the client will also launch the server.
+	client.start();
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate () {
+	if (client) {
+		return client.stop();
+	}
+}
